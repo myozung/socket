cd645895b94ec9c356eda08a976286a483425195
myozung <hyo8215@naver.com>
2025-11-12 19:01:44 +0900

    feat: complete TCP echo server

--- Socket_01/Socket_01/main.cpp
+++ Socket_01/Socket_01/main.cpp
@@ -9,12 +9,20 @@
 int main() {
     const char* HOST = "127.0.0.1";
     const int PORT = 65456;
+    const int BUFFER_SIZE = 1024;
 
     std::cout << "> echo-server is activated" << std::endl;
 
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "socket creation failed" << std::endl;
+        return 1;
+    }
+    
+    int opt = 1;
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1){
+        std::cerr << "setsockopt failed" << std::endl;
+        close(serverSocket);
         return 1;
     }
 
@@ -30,11 +38,13 @@ int main() {
         return 1;
     }
 
-    if (listen(serverSocket, 1) == -1) {
+    if (listen(serverSocket, 5) == -1) {
         std::cerr << "listen failed" << std::endl;
         close(serverSocket);
         return 1;
     }
+    
+    std::cout << "> Listening on " << HOST << ":" << PORT << std::endl;
 
     sockaddr_in clientAddr;
     socklen_t clientAddrSize = sizeof(clientAddr);
@@ -49,6 +59,24 @@ int main() {
               << inet_ntoa(clientAddr.sin_addr)
               << " with Port number "
               << ntohs(clientAddr.sin_port) << std::endl;
+    
+    char buffer[BUFFER_SIZE];
+    while(true){
+        std::memset(buffer, 0, BUFFER_SIZE);
+        ssize_t bytesReceived = read(clientSocket, buffer, BUFFER_SIZE - 1);
+        if (bytesReceived <= 0){
+            std::cout << "> client disconnected" << std::endl;
+            break;
+        }
+        
+        std::cout << "Client:" << buffer << std::endl;
+        
+        ssize_t bytesSent = write(clientSocket, buffer, bytesReceived);
+        if (bytesSent == -1){
+            std::cerr << "send failed" << std::endl;
+            break;
+        }
+    }
 
     close(clientSocket);
     close(serverSocket);
